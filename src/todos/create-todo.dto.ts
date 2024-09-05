5d86537a53b897f60871df3796cc04e75ce961a8
Yamin Faruque <yamin.faruque@sazim.io>
2024-09-05 14:11:11 +0600

    FEAT(FTDA): Add some basic validation via class-validators

--- src/todos/create-todo.dto.ts
+++ src/todos/create-todo.dto.ts
@@ -1,9 +1,22 @@
 import { ETodoPriority } from 'src/common/enums/todo-priority.enum';
+import { IsDateString, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   dueDate: Date;
+
+  @IsNotEmpty()
+  @IsEnum(ETodoPriority)
   priority: ETodoPriority;
+
   isCompleted: boolean;
 }
