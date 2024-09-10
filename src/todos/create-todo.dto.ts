9a05f52f86547052e7cf07e126c8977167d5a705
Yamin Faruque <yamin.faruque@sazim.io>
2024-09-10 14:24:16 +0600

    Improvement(FTDA): Update date validation for createTodos dto

--- src/todos/create-todo.dto.ts
+++ src/todos/create-todo.dto.ts
@@ -1,5 +1,12 @@
 import { ETodoPriority } from '@/common/enums/todo-priority.enum';
-import { IsDateString, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 export class CreateTodoDto {
   @IsNotEmpty()
@@ -10,8 +17,11 @@ export class CreateTodoDto {
   @IsString()
   description!: string;
 
-  @IsDateString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Due date is required.' })
+  @IsDateString({ strict: true })
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'Invalid date format. Use YYYY-MM-DD.',
+  })
   dueDate!: Date;
 
   @IsNotEmpty()
